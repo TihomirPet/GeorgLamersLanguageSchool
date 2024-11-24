dd97badf00422f32af2680574e4e832b9f6fb4b7
TihomirPet <tihomirpetrov@web.de>
2024-11-24 22:04:19 +0100

    video test

---
+++ js/playvideo.js
@@ -0,0 +1,22 @@
+// document.getElementById('myVid').addEventListener('mouseover', function () {
+//   this.play();
+// });
+
+// document.getElementById('myVid').addEventListener('mouseleave', function () {
+//   this.pause();
+// });
+
+// ######
+
+// const video = document.querySelector('.video-container  video');
+// const hoverText = document.querySelector('.video-container .hover-text');
+
+// video.addEventListener('mouseenter', () => {
+//   video.play();
+//   hoverText.classList.remove('active');
+// });
+
+// video.addEventListener('mouseleave', () => {
+//   video.pause();
+//   hoverText.classList.add('active');
+// });
