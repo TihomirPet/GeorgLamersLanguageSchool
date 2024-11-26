4c2b932e31ec8ea70a9a6479cc1612b116344bc7
TihomirPet <tihomirpetrov@web.de>
2024-11-26 22:49:37 +0100

    add filter video

--- js/playvideo.js
+++ js/playvideo.js
@@ -8,15 +8,15 @@
 
 // ######
 
-const video = document.querySelector('.video-container  video');
-const hoverText = document.querySelector('.video-container .hover-text');
+// const video = document.querySelector('.video-container  video');
+// const hoverText = document.querySelector('.video-container .hover-text');
 
-video.addEventListener('mouseenter', () => {
-  video.play();
-  hoverText.classList.remove('active');
-});
+// video.addEventListener('mouseenter', () => {
+//   video.play();
+//   hoverText.classList.remove('active');
+// });
 
-video.addEventListener('mouseleave', () => {
-  video.pause();
-  hoverText.classList.add('active');
-});
+// video.addEventListener('mouseleave', () => {
+//   video.pause();
+//   hoverText.classList.add('active');
+// });
