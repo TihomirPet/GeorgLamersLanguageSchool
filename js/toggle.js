ea97478d13b13587fcf67c99bcd6320ee894ed89
TihomirPet <tihomirpetrov@web.de>
2026-09-07 13:46:20 +0200

    Fix section video mobile view

--- js/toggle.js
+++ js/toggle.js
@@ -1,37 +1,27 @@
+const sections = document.querySelectorAll('.section');
+const navToggle = document.querySelector('.nav-toggle');
 
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const bgClass = entry.target.dataset.bgClass;
 
-// sections.forEach((section) => observer.observe(section));
-// document.addEventListener('navLoaded', () => {
-//   console.log('Scroll Observer startet...');
+        // Reset Klassen
+        navToggle.classList.remove('nav-white', 'nav-dark');
 
-//   const sections = document.querySelectorAll('.section');
-//   const navToggle = document.querySelector('.nav-toggle');
+        // Logik: helle Section → dunkler Button
+        if (bgClass === 'bg-third-white') {
+          navToggle.classList.add('nav-dark');
+        } else {
+          navToggle.classList.add('nav-white');
+        }
+      }
+    });
+  },
+  {
+    threshold: 0.6, // wann Section aktiv wird (60% sichtbar)
+  },
+);
 
-//   if (!navToggle) {
-//     console.error('nav-toggle nicht gefunden!');
-//     return;
-//   }
-
-//   const observer = new IntersectionObserver(
-//     (entries) => {
-//       entries.forEach((entry) => {
-//         if (entry.isIntersecting) {
-//           const bgClass = entry.target.dataset.bgClass;
-
-//           navToggle.classList.remove('nav-white', 'nav-dark');
-
-//           if (bgClass === 'bg-third-white') {
-//             navToggle.classList.add('nav-dark');
-//           } else {
-//             navToggle.classList.add('nav-white');
-//           }
-//         }
-//       });
-//     },
-//     {
-//       threshold: 0.6,
-//     },
-//   );
-
-//   sections.forEach((section) => observer.observe(section));
-// });
+sections.forEach((section) => observer.observe(section));
